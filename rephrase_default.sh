d1f7b8717ba082fc668b6cee1a4336733b339903
guch8017 <guch8017@gmail.com>
2024-01-31 20:16:34 +0800

    Rename bash scripts & Add model argument to scripts

--- rephrase_default.sh
+++ rephrase_default.sh
@@ -2,6 +2,7 @@ export CUDA_VISIBLE_DEVICES=1
 HASHKEY=2023
 
 python rephrase.py \
+    --model-name-or-path TheBloke/Llama-2-7B-GPTQ \
     --input-file results/KGW-KGW/g0.25_d5_selfhash_t4__g0.25_d5_selfhash_t4.jsonl \
     --new-key $HASHKEY \
     --rephraser-file configs/KGW/g0.25_d2_selfhash_t4.yaml \
--- wm_default.sh
+++ wm_default.sh
@@ -1,6 +1,7 @@
 export CUDA_VISIBLE_DEVICES=1
 
 python create_wm_text.py \
-    --generator-file configs/RDW/wsql256.yaml \
-    --detector-file configs/RDW/wsql256.yaml \
+    --model-name-or-path TheBloke/Llama-2-7B-GPTQ \
+    --generator-file configs/SIR/w0_g0.5_d1_z0_context.yaml \
+    --detector-file configs/SIR/w0_g0.5_d1_z0_context.yaml \
     --output-dir results/test
